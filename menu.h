d6d7e03a5338698c33468a5e89c3f59151a3f71b
annymarry51 <134330474+annymarry51@users.noreply.github.com>
2023-10-06 20:08:23 -0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -7,63 +7,72 @@
 /// baixo = 80
 /// ESC   = 27
 
-#include <iostream>
-#include "usuario.h"
-#include "mensagens.h"
-#include "relatorio.h"
+int menuSetasSecundario(int identificaMenu );
+int menuSetasPrincipal(int cor);
 
-//int menu_setas_principal();
+void menuCarteirinha
+{
+    int op;
+    do
+    {
+        op = menuSetasSecundario(1);
+        case 1:
+            //Aqui chama a função de editar dados;
+            break;
+        case 2:
+            //Aqui chama a função de adicionar dados;
+            break;
+    }while(op!=3);
+}
+
+void menuPerfil()
+{
+    int op;
+    do
+    {
+        op = menuSetasSecundario(0);
+        case 1:
+            //Aqui chamar a função de atualizar usuário e senha;
+            break;
+        case 2:
+            //Aqui chamar a função de apagar usuario;
+            break;
+    }while(op!=3);
+}
 
 int menuPrincipal()
 {
-    int escolha;
+    int escolha, color;
+    color = notificacao();
 
     do
     {
-        cout //<< "1- Ver Minha Carteirinha" << endl
-             << "2- Emitir Relatorio" << endl
-             //<< "3- Notificacoes" << endl
-             //<< "4- Meu Perfil" << endl
-             << "5- Sair do Programa" << endl
-             << endl;
-
-        cin >> escolha;
-        cin.ignore();
-
-        system("CLS");
-
+        escolha = menuSetasPrincipal(color);
         switch (escolha)
         {
         case 1:
+            menuSetasSecundario(1);
             break;
         case 2:
             emitirRelatorio();
             break;
         case 3:
-
+            cout << "teste";
             break;
         case 4:
-
-            break;
-        case 5:
-
-            break;
-        case 6:
-
-            break;
-        case 7:
-
+            menuSetasSecundario(0);
             break;
         }
-    } while (escolha != 5 );
+    }
+    while (escolha != 5 );
 }
 
-int menuSetasPrincipal()
+int menuSetasPrincipal(int cor)
 {
-    interface_menu();
-    perfumaria_menu_principal();
+    interface_menu(cor);
+    perfumaria_menu_principal(cor);
 
-    int escolha, enter = 1, linha = 19, aux = 19;
+    int escolha, enter = 1, linha = 14, aux = 14;
 
     do
     {
@@ -77,33 +86,133 @@ int menuSetasPrincipal()
             if (linha < 34)
             {
                 linha += 5;
-                retangulos(MAGENTA, 52, linha, 3, 50);
-                retangulos(15, 51, linha - 1, 3, 50);
+                if(linha == 24 && cor == RED)
+                {
+                    retangulos(MAGENTA, 52, linha, 3, 50);
+                    retangulos(RED, 51, linha - 1, 3, 50);
+                }
+                else
+                {
+                    retangulos(MAGENTA, 52, linha, 3, 50);
+                    retangulos(WHITE, 51, linha - 1, 3, 50);
+                }
 
-                retangulos(0, 52, aux, 3, 50);
-                retangulos(15, 51, aux - 1, 3, 50);
+                if(aux == 24 && cor == RED)
+                {
+                    retangulos(0, 52, aux, 3, 50);
+                    retangulos(RED, 51, aux - 1, 3, 50);
+                }
+                else
+                {
+                    retangulos(0, 52, aux, 3, 50);
+                    retangulos(WHITE, 51, aux - 1, 3, 50);
+                }
                 enter++;
             }
             break;
         case 72:
-            if (linha > 19)
+            if (linha > 14)
             {
                 linha -= 5;
-                retangulos(MAGENTA, 52, linha, 3, 50);
-                retangulos(15, 51, linha - 1, 3, 50);
-
-                retangulos(0, 52, aux, 3, 50);
-                retangulos(15, 51, aux - 1, 3, 50);
+                if(linha == 24 && cor == RED)
+                {
+                    retangulos(MAGENTA, 52, linha, 3, 50);
+                    retangulos(RED, 51, linha - 1, 3, 50);
+                }
+                else
+                {
+                    retangulos(MAGENTA, 52, linha, 3, 50);
+                    retangulos(WHITE, 51, linha - 1, 3, 50);
+                }
+                if(aux == 24 && cor == RED)
+                {
+                    retangulos(0, 52, aux, 3, 50);
+                    retangulos(RED, 51, aux - 1, 3, 50);
+                }
+                else
+                {
+                    retangulos(0, 52, aux, 3, 50);
+                    retangulos(WHITE, 51, aux - 1, 3, 50);
+                }
 
                 enter--;
             }
             break;
         }
-        perfumaria_menu_principal();
+        perfumaria_menu_principal(cor);
         aux = linha;
-    } while (escolha != 27);
+    }
+    while (escolha != 27);
     enter = 4;
     return enter;
 }
 
-#endif 
+
+int menuSetasSecundario(int identificaMenu )
+{
+    int escolha, enter = 1, linha = 24, aux = 24;
+
+    interfaceMenuSecundario();
+
+    if(identificaMenu)
+    {
+        perfumariaMenuCarteirinha();
+    }
+    else
+    {
+        perfumariaMenuPerfil();
+    }
+
+    do
+    {
+        escolha = getch();
+        switch (escolha)
+        {
+        case 13:
+            return enter;
+            break;
+        case 80:
+            if (linha < 34)
+            {
+                linha += 5;
+
+                retangulos(MAGENTA, 52, linha, 3, 50);
+                retangulos(WHITE, 51, linha - 1, 3, 50);
+
+                retangulos(0, 52, aux, 3, 50);
+                retangulos(WHITE, 51, aux - 1, 3, 50);
+
+                enter++;
+            }
+            break;
+        case 72:
+            if (linha > 24)
+            {
+                linha -= 5;
+
+                retangulos(MAGENTA, 52, linha, 3, 50);
+                retangulos(WHITE, 51, linha - 1, 3, 50);
+
+                retangulos(0, 52, aux, 3, 50);
+                retangulos(WHITE, 51, aux - 1, 3, 50);
+
+                enter--;
+            }
+            break;
+        }
+        if(identificaMenu)
+        {
+            perfumariaMenuCarteirinha();
+        }
+        else
+        {
+            perfumariaMenuPerfil();
+        }
+        aux = linha;
+    }
+    while (escolha != 27);
+    enter = 3;
+    return enter;
+}
+
+#endif
