f28383f4a8ef330805f769df1ac64c7ceb539922
NandaCasttro <146202105+NandaCasttro@users.noreply.github.com>
2023-10-05 22:44:28 -0300

    Update main.cpp com as chamadas das bibliotecas

--- main.cpp
+++ main.cpp
@@ -1,72 +1,46 @@
 #include <iostream>
-#include "gconio.h"
-#include <windows.h>
-#include "interface.h"
-#include "Menu_setas.h"
-#include "usuario.h"
+#include <fstream>
 #include <string.h>
 #include <ctime>
+#include <windows.h>
+#include <stdio.h>
+
+#include "gconio.h"
+#include "interface.h"
+#include "usuario.h"
+#include "categoria.h"
+#include "renda.h"
+#include "cofrinho.h"
+#include "relatorio.h"
+#include "menu.h"
+
 
 #define MAX_GASTOS 10
 
 using namespace std;
 
+bool arquivoExiste(const string& arquivoDados) {
+    ifstream arquivo(arquivoDados);
+    return arquivo.good();
+}
+
 int main()
 {
-    Usuario *usuario = new Usuario;
-    Gastos gastos[MAX_GASTOS];
+    Usuario novoUsuario;
 
-    if (arquivoContemDados())
-    {
-        importarUsuarioDoArquivo(usuario);
+    cadastrarUsuario(&novoUsuario);
 
-        cout << "Bem Vindo(a) " << usuario->nome << endl;
-        cout << "Por favor digite sua senha para acessar a sua carteirinha!" << endl;
-        cout << "Esqueceu sua senha?" << endl;
-        cout << "Digite 00 caso tenha esquecido sua senha" << endl;
+    if (!arquivoExiste("dados_financeiros.txt")) {
 
-        string opcao;
+        entradaDeRenda();
+        entradaDeCategorias();
+        entradaDeCofrinho();
 
-        while (true)
-        {
-            getline(cin, opcao);
-            if (opcao == "00")  //passível a modificações
-            {
-                cadastrarUsuario(usuario);
-                system("CLS");
-                break;
-            }
-            else
-            {
-                if (usuario->senha == opcao)
-                {
-                    system("CLS");
-                    cout << "Logado com sucesso. Utilize a sua carteirinha a vontade!" << endl;
-                    break;
-                }
-                else
-                {
-                    system("CLS");
-                    cout << "Senha incorreta! Digite sua senha novamente. Digite 00 caso tenha esquecido sua senha" << endl;
-                    continue;
-                }
-            }
-        }
-    }
-    else
-    {
-        cadastrarUsuario(usuario);
     }
 
-    
+    menuPrincipal();
 
-    menu_principal(usuario, gastos);
-    // menu_setas_principal();
-
-    getch();
-    // tela_final();
-
-    delete usuario;
 
     return 0;
 }
+
