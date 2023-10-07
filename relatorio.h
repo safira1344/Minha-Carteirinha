33c91048dc97571ab22e7d9b363b18d201a3b0d1
NandaCasttro <146202105+NandaCasttro@users.noreply.github.com>
2023-10-07 14:41:56 -0300

    Update relatorio.h

--- relatorio.h
+++ relatorio.h
@@ -1,108 +1,202 @@
 #ifndef RELATORIO_H_INCLUDED
 #define RELATORIO_H_INCLUDED
 
-#include <iostream>
-#include <fstream>
-#include <string>
+// Assinatura das funções
+void extrairRenda(string &arquivoRenda, string &data, string &mesExtenso);
+void extrairDespesas(string &arquivoDespesa, string &data, string &mesExtenso);
+void extrairCofrinho(string &arquivoCofre, string &data, string &mesExtenso);
+string converterMes(string data);
 
-using namespace std;
+// Funcao main dessa biblioteca
+void emitirRelatorio()
+{
+    string data;
+    cout << "Digite o mes desejado no formato mm-aaaa: ";
+    cin >> data;
 
-struct UsuarioRelatorio {
-    string nome;
-    string cpf;
-};
-
-struct RendaRelatorio {
-    int mesCadastro;
-    string nome;
-    double valor;
-};
-
-struct DespesaRelatorio {
-    int mesCadastro;
-    string categoria;
-    double valor;
-};
-
-void extrairUsuario(const string& arquivoUsuario) {
-    ifstream arquivo(arquivoUsuario);
-
-    if (arquivo.is_open()) {
-        string nomeUsuario, cpfUsuario;
-        while (arquivo >> nomeUsuario >> cpfUsuario) {
-            cout << "Nome: " << nomeUsuario << ", CPF: " << cpfUsuario << endl;
-        }
-        arquivo.close();
-    } else {
-        cout << "Erro ao abrir o arquivo de usuários." << endl;
+    if (data.size() != 7 || data[2] != '-')
+    {
+        cout << "Formato de mês inválido. Use o formato mm-aaaa." << endl;
+        return;
     }
+
+    string mesExtenso = converterMes(data);
+
+    string arquivoRenda = "dados_renda.txt";
+    string arquivoDespesa = "dados_financeiros.txt";
+    string arquivoCofre = "dados_cofre.txt";
+
+    Usuario usuario = importarUsuario();
+    string mesEmissao = obterMesAtual();
+
+        cout << ""<< endl;
+        cout << "---- RELATORIO DE CONTROLE DE GASTOS DO MES " << data << " ----"<< endl << endl;
+        cout << "Nome: " << usuario.nome << endl;
+        cout << "CPF: " << usuario.cpf << endl << endl;
+        cout << "Mes de emissao do Relatorio: " << mesEmissao << endl << endl;
+        extrairRenda(arquivoRenda, data, mesExtenso);
+        extrairDespesas(arquivoDespesa, data, mesExtenso);
+        extrairCofrinho(arquivoCofre, data, mesExtenso);
+        cout << "-------------------------------------------------------" << endl << endl;
+
 }
 
-double calculoRenda(const string& arquivoRenda, int mesDesejado) {
+// Função que soma todas as rendas de dentro do arquivo de renda
+void extrairRenda(string &arquivoRenda, string &data, string &mesExtenso)
+{
     ifstream arquivo(arquivoRenda);
-    double totalRendas = 0.0;
 
-    if (arquivo.is_open()) {
-        int mesRenda;
-        string nomeRenda;
-        double valorRenda;
-        while (arquivo >> mesRenda >> nomeRenda >> valorRenda) {
-            if (mesRenda == mesDesejado) {
-                totalRendas += valorRenda;
-            }
-        }
-        arquivo.close();
-    } else {
+    if (!arquivo.is_open())
+    {
         cout << "Erro ao abrir o arquivo de rendas." << endl;
     }
 
-    return totalRendas;
-}
+    double totalRendas = 0.0;
+    string linha;
 
-void extrairDespesas(const string& arquivoDespesa, int mesDesejado) {
-    ifstream arquivo(arquivoDespesa);
-    double totalDespesasMes = 0.0;
+    while (getline(arquivo, linha))
+    {
+        if (linha.find("Mes de Cadastro: " + data) != string::npos)
+        {
+            string nomeRenda;
+            double valorRenda = 0.0;
 
-    if (arquivo.is_open()) {
-        int mesDespesa;
-        string categoriaDespesa;
-        double valorDespesa;
-
-        while (arquivo >> mesDespesa >> categoriaDespesa >> valorDespesa) {
-            if (mesDespesa == mesDesejado) {
-                cout << "Categoria: " << categoriaDespesa << ", Valor: " << valorDespesa << endl;
-                totalDespesasMes += valorDespesa;
+            while (getline(arquivo, linha) && !linha.empty())
+            {
+                if (linha.find("Nome: ") != string::npos)
+                {
+                    nomeRenda = linha.substr(linha.find("Nome: ") + 6);
+                }
+                else if (linha.find("Valor: R$ ") != string::npos)
+                {
+                    valorRenda = stod(linha.substr(linha.find("Valor: R$ ") + 9));
+                }
             }
-        }
-        arquivo.close();
 
-        cout << "Total de Despesas em " << mesDesejado << ": " << totalDespesasMes << endl;
-    } else {
-        cout << "Erro ao abrir o arquivo de despesas." << endl;
+            totalRendas += valorRenda;
+        }
     }
+
+    arquivo.close();
+
+    cout << "RENDA" << endl;
+    cout << "- Renda Total de " << mesExtenso << ": " << totalRendas << endl << endl;
 }
 
-// Função main
-int emitirRelatorio() {
+// Função que importa o nome e valor das despesas, e faz a soma delas
+void extrairDespesas(string &arquivoDespesa, string &data, string &mesExtenso)
+{
+    ifstream arquivo(arquivoDespesa);
 
-    int mesDesejado;
-    cout << "Digite o mês desejado (1 a 12): ";
-    cin >> mesDesejado;
+    if (!arquivo.is_open())
+    {
+        cout << "Erro ao abrir o arquivo de despesas." << endl;
+        return;
+    }
 
-    double totalRendas;
+    double totalDespesasMes = 0.0;
+    string linha;
 
-    string arquivoUsuario = "dados_usuario.txt";
-    string arquivoRenda = "dados_renda.txt";
-    string arquivoDespesa = "dados_financeiros.txt";
+    cout << "DESPESAS" << endl;
 
-    extrairUsuario(arquivoUsuario);
+    while (getline(arquivo, linha))
+    {
+        if (linha.find("Mes de Cadastro: " + data) != string::npos)
+        {
+            string categoriaDespesa;
+            double valorDespesa = 0.0;
 
-    totalRendas = calculoRenda(arquivoRenda, mesDesejado);
-    cout << "Total de Rendas no mês " << mesDesejado << ": " << totalRendas << endl;
+            while (getline(arquivo, linha) && !linha.empty())
+            {
+                if (linha.find("Categoria: ") != string::npos)
+                {
+                    categoriaDespesa = linha.substr(linha.find("Categoria: ") + 11);
+                }
+                else if (linha.find("Valor: ") != string::npos)
+                {
+                    valorDespesa = stod(linha.substr(linha.find("Valor: ") + 7));
+                }
+            }
 
-    extrairDespesas(arquivoDespesa, mesDesejado);
+            cout << "- Despesa: " << categoriaDespesa << " - " << valorDespesa << endl;
+            totalDespesasMes += valorDespesa;
+        }
+    }
 
-    return 0;
+    arquivo.close();
+
+    cout << "" << endl;
+    cout << "Despesa total em " << mesExtenso << ": " << totalDespesasMes << endl << endl;
+}
+
+// Função que importa o nome e valor dos cofres, e faz a soma deles
+void extrairCofrinho(string &arquivoCofre, string &data, string &mesExtenso)
+{
+    ifstream arquivo(arquivoCofre);
+
+    if (!arquivo.is_open())
+    {
+        cout << "Erro ao abrir o arquivo do cofre." << endl;
+        return;
+    }
+
+    double totalGuardado = 0.0;
+    string linha;
+
+    cout << "DINHEIRO GUARDADO" << endl;
+
+    while (getline(arquivo, linha))
+    {
+        if (linha.find("Mes de Cadastro: " + data) != string::npos)
+        {
+            string nomeCofre;
+            double valorCofre = 0.0;
+
+            while (getline(arquivo, linha) && !linha.empty())
+            {
+                if (linha.find("Nome: ") != string::npos)
+                {
+                    nomeCofre = linha.substr(linha.find("Nome: ") + 6);
+                }
+                else if (linha.find("Valor: ") != string::npos)
+                {
+                    valorCofre = stod(linha.substr(linha.find("Valor: ") + 7));
+                }
+            }
+
+            cout << "- Cofre: " << nomeCofre << " - " << valorCofre << endl;
+            totalGuardado += valorCofre;
+        }
+    }
+
+    arquivo.close();
+
+    cout << "" << endl;
+    cout << "Valor total guardado em " << mesExtenso << ": " << totalGuardado << endl << endl;
+}
+
+// Função que escreve o mes por extenso
+string converterMes(string data)
+{
+    string numeroMes = data.substr(0, 2);
+    string ano = data.substr(3);
+
+    string nomeMes;
+    if (numeroMes == "01") nomeMes = "janeiro";
+    else if (numeroMes == "02") nomeMes = "fevereiro";
+    else if (numeroMes == "03") nomeMes = "março";
+    else if (numeroMes == "04") nomeMes = "abril";
+    else if (numeroMes == "05") nomeMes = "maio";
+    else if (numeroMes == "06") nomeMes = "junho";
+    else if (numeroMes == "07") nomeMes = "julho";
+    else if (numeroMes == "08") nomeMes = "agosto";
+    else if (numeroMes == "09") nomeMes = "setembro";
+    else if (numeroMes == "10") nomeMes = "outubro";
+    else if (numeroMes == "11") nomeMes = "novembro";
+    else if (numeroMes == "12") nomeMes = "dezembro";
+    else nomeMes = "mês inválido";
+
+    return nomeMes + " de " + ano;
 }
 
 #endif
