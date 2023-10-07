9869a91002fbc189bfec5bcc4023474816422576
NandaCasttro <146202105+NandaCasttro@users.noreply.github.com>
2023-10-07 14:45:34 -0300

    Update cofrinho.h

--- cofrinho.h
+++ cofrinho.h
@@ -1,50 +1,117 @@
 #ifndef COFRINHO_H_INCLUDED
 #define COFRINHO_H_INCLUDED
 
-#include <iostream>
-#include <fstream>
-#include <string>
+// Assinatura das funções
+double calcularEmprestimo();
+void preverTempo();
 
-using namespace std;
-
-// Função main
-int entradaDeCofrinho() {
-    char opcao;
+// Funcao main dessa biblioteca
+void guardarDinheiro()
+{
     cout << "E pra finalizar..." << endl << endl;
+
+    char opcao;
     cout << "Deseja guardar dinheiro? (S/N): ";
     cin >> opcao;
 
-    if (opcao == 'S' || opcao == 's'){
-        do {
-            string nomeCofrinho;
-            double quantidadeDinheiro;
+    while (opcao == 'S' || opcao == 's')
+    {
+        string nomeCofrinho;
+        cout << "Informe o nome do cofrinho: ";
+        cin.ignore();
+        getline(cin, nomeCofrinho);
 
-            cout << "Informe o nome do cofrinho: ";
-            cin >> nomeCofrinho;
+        double quantidadeDinheiro;
+        cout << "Por favor, insira a quantidade de dinheiro que deseja guardar: R$ ";
+        cin >> quantidadeDinheiro;
 
-            cout << "Informe a quantidade de dinheiro que deseja guardar: R$ ";
-            cin >> quantidadeDinheiro;
-            cout <<""<< endl;
+        ofstream arquivo("dados_cofre.txt", ios::app);
 
-            ofstream arquivo("dados_cofre.txt");
+        if (!arquivo.is_open())
+        {
+            cout << "Erro ao abrir o arquivo, desculpe" << endl;
+            return;
+        }
 
-            if (arquivo.is_open()) {
-                arquivo << "Nome do cofrinho: " << nomeCofrinho << endl;
-                arquivo << "Valor Guardado: R$ " << quantidadeDinheiro << endl;
+        arquivo << "Nome: " << nomeCofrinho << endl
+                << "Valor: " << quantidadeDinheiro << endl;
 
-                arquivo.close();
+        arquivo.close();
 
-                cout << "Os dados foram salvos com sucesso!!!" << endl << endl;
-            } else {
-                cout << "Erro ao abrir o arquivo, desculpe" << endl;
-            }
+        cout << "Os dados foram salvos com sucesso!!!" << endl;
 
-            cout << "Deseja guardar mais dinheiro para outra coisa? (S/N): ";
-            cin >> opcao;
-
-        } while (opcao == 'S' || opcao == 's');
+        cout << "Deseja guardar mais dinheiro? (S/N): ";
+        cin >> opcao;
     }
-    return 0;
+}
+
+// Função  para prever em quanto tempo o usuário terá uma certa quantidade de dinheiro
+void preverTempo()
+{
+    double valorAlvo;
+    double economiaMensal;
+
+    cout << "Informe o valor alvo que deseja alcançar: R$ ";
+    cin >> valorAlvo;
+
+    cout << "Informe a economia mensal que deseja fazer: R$ ";
+    cin >> economiaMensal;
+
+    if (valorAlvo <= 0 || economiaMensal <= 0)
+    {
+        cout << "Informe um valor alvo e uma economia mensal válidos maiores que zero." << endl;
+        return;
+    }
+
+    double saldoAtual = 0;
+    int meses = 0;
+
+    while (saldoAtual < valorAlvo)
+    {
+        saldoAtual += economiaMensal;
+        meses++;
+    }
+
+    cout << "Com uma economia de R$ " << economiaMensal << " por mês, levará aproximadamente " << meses << " meses para alcançar o valor alvo de R$ " << valorAlvo << "." << endl;
+}
+
+// Função q calcula os ganhos ao final de um empréstimo com juros mensais crescentes
+double calcularEmprestimo()
+{
+    double valorEmprestado;
+    double taxaJuros;
+    int meses;
+
+    cout << "Informe o valor que deseja emprestar: R$ ";
+    cin >> valorEmprestado;
+
+    cout << "Informe a taxa de juros mensal (em decimal): ";
+    cin >> taxaJuros;
+
+    if (valorEmprestado <= 0 || taxaJuros <= 0)
+    {
+        cout << "Informe um valor de empréstimo e uma taxa de juros válidos maiores que zero." << endl;
+        return 0.0;
+    }
+
+    cout << "Informe o número de meses para o empréstimo: ";
+    cin >> meses;
+
+    if (meses <= 0)
+    {
+        cout << "Informe um número de meses válido maior que zero." << endl;
+        return 0.0;
+    }
+
+    if (meses == 1)
+    {
+        return valorEmprestado * (1 + taxaJuros);
+    }
+    else
+    {
+        double saldoMesAnterior = calcularEmprestimo();
+        return saldoMesAnterior * (1 + taxaJuros);
+    }
 }
 
 #endif
